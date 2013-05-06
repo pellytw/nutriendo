70348f4bf960e8badd792d5f21b8e49297819bac
Pelly <marcelo.eduardo.gonzalez@gmail.com>
2013-05-06 13:13:36 -0300

    arrancando con los permisos

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,7 +1,7 @@
 class Ability
   include CanCan::Ability
 
-   def initialize(user)
+#   def initialize(user)
 #    if user then
 #	    user.roles.each do |role|
 #	      role.permissions.each do |permission|
@@ -10,5 +10,23 @@ class Ability
 #	 end
 #	end
 
-   end
+#   end
+
+def initialize(user)
+
+	user ||= User.new # guest user
+    if user.role? :admin
+      can :manage, :all
+    elsif user.role? :nutriendo
+    	can :read, :all
+
+      #Escuelas
+      #can :update, [Escuela]
+      can :read, [Escuela]
+      #can :create, [Escuela]
+      
+
+    end
+end
+
 end
